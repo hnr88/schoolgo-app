144c0b692efb766f7cd8d7df02b93fa62fd696ea
Nagy Hunor <nagyhunorferenc@gmail.com>
2026-05-24 20:15:22 +0300

    chore(next): enable cacheComponents in next config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  cacheComponents: true,
   allowedDevOrigins: [
     '192.168.68.55',
     '*.local',
